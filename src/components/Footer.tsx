6d5cc6b3579f2bf044bac0b832dfc9f5269c49d5
Ulisses Malanski <malanskiwork@gamil.com>
2023-08-16 16:35:12 -0300

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box } from '@chakra-ui/react'
+import { Flex, Box, Text } from '@chakra-ui/react'
 import {
   AiFillFacebook,
   AiFillGithub,
@@ -8,36 +8,41 @@ import {
 
 export const Footer = () => {
   return (
-    <Flex
-      bg="background.darkBlue"
-      align="center"
-      justify="space-between"
-      padding={6}
-    >
-      <Box as="a" href="https://github.com/malanski" title="Github Profile">
-        <AiFillGithub />
-      </Box>
-      <Box
-        as="a"
-        href="https://www.linkedin.com/in/ulisses-malanski/"
-        title="Linkedin Profile"
+    <Box>
+      <Flex
+        bg="background.darkBlue"
+        align="center"
+        justify="space-between"
+        padding={6}
       >
-        <AiFillLinkedin />
-      </Box>
-      <Box
-        as="a"
-        href="https://www.facebook.com/ulisses.malanski"
-        title="FaceBook Profile"
-      >
-        <AiFillFacebook />
-      </Box>
-      <Box
-        as="a"
-        href="https://www.instagram.com/malanskiart/"
-        title="Instagram"
-      >
-        <AiFillInstagram />
-      </Box>
-    </Flex>
+        <Box as="a" href="https://github.com/malanski" title="Github Profile">
+          <AiFillGithub />
+        </Box>
+        <Box
+          as="a"
+          href="https://www.linkedin.com/in/ulisses-malanski/"
+          title="Linkedin Profile"
+        >
+          <AiFillLinkedin />
+        </Box>
+        <Box
+          as="a"
+          href="https://www.facebook.com/ulisses.malanski"
+          title="FaceBook Profile"
+        >
+          <AiFillFacebook />
+        </Box>
+        <Box
+          as="a"
+          href="https://www.instagram.com/malanskiart/"
+          title="Instagram"
+        >
+          <AiFillInstagram />
+        </Box>
+      </Flex>
+      <Text bgColor="black" color="white" textAlign="center">
+        Desenvolvido por Ulisses Malanski
+      </Text>
+    </Box>
   )
 }
